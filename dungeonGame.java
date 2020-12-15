22e1fef4aae43d451dba1368ef14dc629c82f4d9
Christian Dohler <christian.dohler@mynbps.org>
2020-12-15 16:34:08 +0000

    Added variables

---
+++ dungeonGame.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class DungeonGame {
+  public static void main(String[] args) 
+  {
+    Scanner input = Scanner(System.in);
+    Random rand = new Randon();
+
+
+  String() enemies = ("Vampire", "Mind Flayer", "Dragon")
+  int EnemyMaxHealth = 80;
+  int EnemyMaxDamageDealt = 20;
+  int EnemyHealItself = 20;
+
+  }
+}
