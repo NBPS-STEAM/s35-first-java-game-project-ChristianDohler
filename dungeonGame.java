378e6fdb35b434b2061bbbc5813d81cda652eda1
Christian Dohler <christian.dohler@mynbps.org>
2020-12-17 05:31:54 +0000

    started the game with a while loop that shows that the game is running until we stop it. It is labeled so we can iterate back to it later

--- dungeonGame.java
+++ dungeonGame.java
@@ -1,17 +1,33 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class DungeonGame {
-  public static void main(String[] args) 
-  {
-    Scanner input = Scanner(System.in);
-    Random rand = new Randon();
+public class dungeonGame {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    Random rand = new Random();
 
+    // enemy
+    String[] enemies = { "Ghoul", "Mind Flayer", "Goblin" };
+    int EnemyMaxHealth = 80;
+    int EnemyMaxDamageDealt = 20;
+    int EnemyHealthPotions = 1;
+    int EnemyRegeneration = 20;
 
-  String() enemies = ("Vampire", "Mind Flayer", "Dragon")
-  int EnemyMaxHealth = 80;
-  int EnemyMaxDamageDealt = 20;
-  int EnemyHealItself = 20;
+    // player
+    int healt = 100;
+    int attackDamage = 45;
+    int healthPotions = 3;
+    int maxRegeneration = 25;
+
+    boolean working = true;
+
+    System.out.println("\t DUNGEON ADVENTURE   ");
+
+    GAME: while (working) {
+
+      System.out.println("Hello Mortal, welcome to the dungeon!");
+
+    }
 
   }
 }
