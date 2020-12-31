303bd50ee62a60a2f820f4a5e6251fe22c6c7d41
Christian Dohler <christian.dohler@mynbps.org>
2020-12-31 05:36:30 +0000

    created while loop with user input and that starts and keeps the adventure going as long as the enemies health is above 0

--- dungeonGame.java
+++ dungeonGame.java
@@ -24,12 +24,53 @@ public class dungeonGame {
 
     System.out.println("|_________________________|");
 
+    // used a for loop as a countdown
+    for(int i = 3; i > 0; i--)
+      {
+        System.out.println(i);  
+      }
+
+    //labeled the loop to iterate back to it later
     GAME: while (working) {
 
       //gives enemy random health between 1 to 100
       int enemyHealth = rand.nextInt(enemyMaxHealth);
+
       //picks a random enemy from the array written earlier using the string method .length
       String enemy = enemies[rand.nextInt(enemies.length)];
+      System.out.println(enemy "has appeared!");
+
+      //create loop that starts game and asks for user input
+      while(enemyHealth > 1)
+      {
+        System.out.println("Your Health: " + maxHealth);
+        System.out.println(enemy + "'s Health: " + enemyHealth);
+        System.out.println("choose your way to proceed by typing number before specified action");
+        System.out.println("1. Attack");
+        System.out.println("2. Run") 
+
+        String input = input.nextLine();
+        if(input.equals("1"))
+        {
+          int damageDealt = rand.nextInt(attackDamage);
+          int damageTaken = rand.nextInt(enemyMaxDamageDealt);
+          int enemyHealthBack = rand.nextInt(enemyRegeneration);
+          int regainHealth = rand.nextInt(maxRegeneration);
+
+          enemyHealth -= damageDealt;
+          maxHealth -= damageTaken;
+          enemyHealth += enemyHealthBack;
+
+
+        }
+
+      
+
+      }
+
+      
+      
+
 
     }
 
